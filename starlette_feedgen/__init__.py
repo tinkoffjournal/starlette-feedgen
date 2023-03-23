30a3efc905d326a47265c798f8d677ceaed63235
Alexandre Alapetite <alexandre@alapetite.fr>
2023-03-23 22:01:00 +0100

    Update starlette_feedgen/__init__.py
    Same new quote style

--- starlette_feedgen/__init__.py
+++ starlette_feedgen/__init__.py
@@ -5,5 +5,5 @@ adapted from Django syndication feed framework
 
 from .feed import FeedEndpoint, FeedDoesNotExist
 
-__all__ = ("FeedEndpoint","FeedDoesNotExist",)
-__version__ = "0.1.4"
+__all__ = ('FeedEndpoint','FeedDoesNotExist',)
+__version__ = '0.1.4'
